ee488d5331da3041107235871e7f27f3b18f6b08
JustroX <thepiesaresquared@gmail.com>
2022-01-24 23:02:36 +0800

    refactor: remove keystore

--- src/setup.ts
+++ src/setup.ts
@@ -20,12 +20,10 @@ export async function setup() {
     'Please provide Pinata API key (See https://docs.pinata.cloud/#your-api-keys):',
   );
   const pinata_secret = await ask('Please provide Pinata secret:');
-  const master_password = await ask('Please provide application master key:');
 
-  const file = `PINATA_API_KEY=${pinata_key}\nPINATA_SECRET_KEY=${pinata_secret}\nMASTER_KEY=${master_password}`;
+  const file = `PINATA_API_KEY=${pinata_key}\nPINATA_SECRET_KEY=${pinata_secret}`;
   await writeFile(process.cwd() + '/.env', file);
 
   process.env.PINATA_API_KEY = pinata_key;
   process.env.PINATA_SECRET_KEY = pinata_secret;
-  process.env.MASTER_KEY = master_password;
 }
