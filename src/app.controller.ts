fae8feaa77f2678e9730ec2fd7ad4157b33dde87
JustroX <thepiesaresquared@gmail.com>
2022-05-21 21:57:33 +0800

    feat: peers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { IPFSNode } from './shared/ipfs-node';
 
 @Controller()
 export class AppController {
   @Get()
   get(@Res() res: Response) {
     res.redirect('/index.html');
+  }
+
+  @Get('/api/peers')
+  getPeers() {
+    return IPFSNode.peers;
   }
 }
